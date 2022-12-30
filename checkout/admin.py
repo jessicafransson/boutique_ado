1c9454c67cb1f38f92aa6780cb5e40785adef636
Jessica Fransson <jessicafransson@outlook.com>
2022-12-30 14:51:47 +0000

    started on admin file for checkout

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = ('order_number', 'date',
+                        'delivery_cost', 'order_total',
+                        'grand_total',)
