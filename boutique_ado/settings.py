16c54e7832e995c5fa76bcc302b886113bb887eb
Jessica Fransson <jessicafransson@outlook.com>
2023-01-05 20:49:27 +0000

    added cache control

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -180,7 +180,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    # bucket config
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
+    # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'jessicafransson-boutique-ado'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
@@ -196,6 +202,7 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLS in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
+
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
